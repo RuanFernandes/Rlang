457e9ef97d77630327b3bc800a81888fad7eacfc
Ruan <ruan_guimaraes2004@hotmail.com>
2023-02-23 10:07:54 -0300

    Lexer

--- rc/core/Lexer.cs
+++ rc/core/Lexer.cs
@@ -4,19 +4,23 @@ using System.Collections.Generic;
 
 namespace rc.core
 {
+    public class Token
+    {
+        public TokenType Type { get; set; }
+        public string Value { get; set; }
+
+        public Token(TokenType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
     public class Lexer
     {
-        public class Token
-        {
-            public TokenType Type { get; set; }
-            public string Value { get; set; }
 
-            public Token(TokenType type, string value)
-            {
-                Type = type;
-                Value = value;
-            }
-        }
+        private readonly string _input;
+        private readonly List<Token> _tokens;
+        private int _position;
 
         private static readonly Dictionary<string, TokenType> TokenTypes = new Dictionary<string, TokenType>
         {
@@ -73,5 +77,51 @@ namespace rc.core
             {"/*", TokenType.Other_Comment},
             {"*/", TokenType.Other_Comment}
         };
+
+        public Lexer(string input)
+        {
+            _input = input;
+            _tokens = new List<Token>();
+            _position = 0;
+        }
+
+        public List<Token> Tokenize()
+        {
+            while (_position < _input.Length)
+            {
+                var token = GetNextToken();
+                if (token != null)
+                {
+                    _tokens.Add(token);
+                }
+                else
+                {
+                    throw new Exception("Unknown token");
+                }
+            }
+
+            return _tokens;
+        }
+
+        private Token? GetNextToken()
+        {
+            while (_position < _input.Length && char.IsWhiteSpace(_input[_position]))
+                _position++;
+
+            if (_position >= _input.Length)
+                return null;
+
+            foreach (var tokenType in TokenTypes)
+            {
+                if (_input.Substring(_position).StartsWith(tokenType.Key))
+                {
+                    var token = new Token(tokenType.Value, tokenType.Key);
+                    _position += tokenType.Key.Length;
+                    return token;
+                }
+            }
+
+            return null;
+        }
     }
 }
